30873a68d605e4d0b2cc41f2f25913f3d10e2e23
misachi <misachibrian@gmail.com>
2021-03-19 20:19:15 +0300

    Add search capabilities to tree
    Add a fee data on main function to test out tree

--- btree.go
+++ btree.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 var DEGREE = 5
 
 type Node struct {
@@ -57,8 +55,8 @@ func (n *Node) splitNode(degree int, idx int) {
 	for cnt := n.NumKeys; cnt < idx; cnt-- {
 		n.Keys[cnt+1] = n.Keys[cnt]
 	}
-	n.Keys[degree] = fullNode.Keys[newNode.NumKeys+1]
-	n.NumKeys = n.NumKeys + 1
+	n.Keys[idx] = fullNode.Keys[degree-1]
+	n.NumKeys += 1
 }
 
 type BTree struct {
@@ -67,8 +65,8 @@ type BTree struct {
 }
 
 func (btree *BTree) insertLeaf(n *Node, key int) {
-	nodeIdx := n.NumKeys
-	for nodeIdx >= 1 && key < n.Keys[nodeIdx] {
+	nodeIdx := n.NumKeys - 1
+	for nodeIdx >= 0 && key < n.Keys[nodeIdx] {
 		n.Keys[nodeIdx+1] = n.Keys[nodeIdx]
 		nodeIdx--
 	}
@@ -80,8 +78,8 @@ func (btree *BTree) insertNonFull(n *Node, key int) {
 	if n.Leaf {
 		btree.insertLeaf(n, key)
 	} else {
-		nodeIdx := n.NumKeys
-		for nodeIdx >= 1 && key < n.Keys[nodeIdx] {
+		nodeIdx := n.NumKeys - 1
+		for nodeIdx >= 0 && key < n.Keys[nodeIdx] {
 			nodeIdx--
 		}
 		nodeIdx += 1
@@ -99,10 +97,10 @@ func (btree *BTree) Insert(key int) {
 	root := btree.Root
 	if root.NumKeys == maxKeys(btree.Degree) {
 		newNode := CreateNewNode(btree.Degree)
+		btree.Root = newNode
 		newNode.NumKeys = 0
 		newNode.Leaf = false
 		newNode.Children[0] = root
-		btree.Root = newNode
 		newNode.splitNode(btree.Degree, 0)
 		btree.insertNonFull(newNode, key)
 	} else {
@@ -112,17 +110,23 @@ func (btree *BTree) Insert(key int) {
 
 func (btree *BTree) Search(root *Node, key int) (*Node, int) {
 	idx := 0
-	for idx < root.NumKeys && key < root.Keys[idx] {
+	for idx < root.NumKeys && key > root.Keys[idx] {
 		idx++
 	}
-	fmt.Println(root.Keys[idx])
+
+	if idx >= root.NumKeys {
+		idx--
+	}
+
 	if key == root.Keys[idx] {
 		return root, idx
-	} else if root.Leaf && key != root.Keys[idx] {
+	}
+
+	if root.Leaf {
 		return nil, -1
 	} else {
-		if idx == root.NumKeys {
-			idx -= 1
+		if key > root.Keys[idx] {
+			idx++
 		}
 		return btree.Search(root.Children[idx], key)
 	}
--- main.go
+++ main.go
@@ -3,13 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	degree := 5
+	degree := 4
 	root := CreateNewNode(degree)
+	root.Keys[0] = 3
+	root.NumKeys += 1
 	bt := BTree{Root: root, Degree: degree}
+	bt.Insert(6)
 	bt.Insert(4)
 	bt.Insert(5)
+	bt.Insert(2)
+	bt.Insert(7)
+	bt.Insert(8)
+	bt.Insert(9)
+	bt.Insert(10)
+	bt.Insert(1)
+	bt.Insert(11)
+	bt.Insert(12)
+	bt.Insert(2)
+	bt.Insert(22)
 
-	node, idx := bt.Search(bt.Root, 5)
+	/* We check the root and child nodes*/
+	fmt.Println(bt.Root.Keys)
+	fmt.Println(bt.Root.Children[0].Keys, bt.Root.Children[0].NumKeys)
+	fmt.Println(bt.Root.Children[1].Keys, bt.Root.Children[1].NumKeys)
+	node, idx := bt.Search(bt.Root, 2)
 	if node != nil {
 		fmt.Println(node.Keys[idx])
 	} else {
